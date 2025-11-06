7b9ec382fd83d9b4a3f66baa3f374343604d7845
Thiago Prazeres <thiagoprazeres@gmail.com>
2025-11-06 20:37:33 -0300

    SSG

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,16 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { equipamentosCategoriasData } from './data/equipamentos-categorias-data';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'equipamentos/:slug',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      return equipamentosCategoriasData.map((categoria) => ({ slug: categoria.slug }));
+    },
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
