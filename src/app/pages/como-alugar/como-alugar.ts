3bc883eb7697375b3f1ff9927750ac1c70115b7a
Thiago Prazeres <thiagoprazeres@gmail.com>
2025-11-06 22:52:09 -0300

    ajustes

--- src/app/pages/como-alugar/como-alugar.ts
+++ src/app/pages/como-alugar/como-alugar.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LucideAngularModule, Mail } from 'lucide-angular';
 
 @Component({
   selector: 'app-como-alugar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LucideAngularModule],
   templateUrl: './como-alugar.html',
   styleUrl: './como-alugar.css',
 })
-export class ComoAlugarPage {}
+export class ComoAlugarPage {
+  readonly Mail = Mail;
+}
